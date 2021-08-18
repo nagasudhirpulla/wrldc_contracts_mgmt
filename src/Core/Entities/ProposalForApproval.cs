8b754b89da243d375d696a5197202047bca900e4
sanjeet <sanjeetchaudhary5@gmail.com>
2021-08-18 11:55:37 +0530

    oneToManyProposal

---
+++ src/Core/Entities/ProposalForApproval.cs
@@ -0,0 +1,18 @@
+﻿using Core.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class ProposalForApproval: BaseEntity
+    {
+        public Notesheet Notesheet { get; set; }
+        public int NotesheetId { get; set; }
+
+        public string ProposalOption { get; set; }
+
+    }
+}
---
+++ src/Infra/Persistence/Configurations/ProposalConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Persistence.Configurations
+{
+    public class ProposalForApprovalConfiguration : IEntityTypeConfiguration<ProposalForApproval>
+    {
+        public void Configure(EntityTypeBuilder<ProposalForApproval> builder)
+        {
+
+            builder.Property(b => b.ProposalOption)
+                .IsRequired()
+                .HasMaxLength(250);
+
+            builder
+                .HasIndex(sp => new { sp.NotesheetId, sp.ProposalOption })
+                .IsUnique();
+        }
+    }
+}
