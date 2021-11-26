576420c904b93c46542aedc24129b1034e2703a5
Dheeraj Gupta <dheeraj.g@wrldc.local>
2021-11-26 17:06:02 +0530

    DepartmentAdded

--- src/WebApp/Startup.cs
+++ src/WebApp/Startup.cs
@@ -47,16 +47,18 @@ namespace WebApp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IMediator mediator)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+            //else
+            //{
+            //    app.UseExceptionHandler("/Error");
+            //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            //    app.UseHsts();
+            //}
+            app.UseDeveloperExceptionPage();
+            app.UseExceptionHandler("/Error");
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
